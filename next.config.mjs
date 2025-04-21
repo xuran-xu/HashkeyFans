08601d87cfa383fbd9cd5f1448deaa58e1249313
SpectreMercury <francislee920217@gmail.com>
2025-04-21 18:21:13 +0800

    update: remove eslint build check

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        // ESLint 检查不会导致构建失败
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
